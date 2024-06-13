f46dc2900ee5a955e64e31c8bd6bacb9a00e2863
vleandrox <victor.alarcon.t@hotmail.com>
2024-06-13 13:07:16 -0500

    UpdateHooksRepaso

--- virtual-store/src/components/CartCard.tsx
+++ virtual-store/src/components/CartCard.tsx
@@ -1,19 +1,42 @@
-export default function CartCard({title,color,price,image}) {
+import { useRef } from "react"
+
+export default function CartCard(product) {
+    const { id, title, image, price, color, quantity } = product;
+    const units = useRef();
+    const manageUnits = () => {
+        const productsOnCart = JSON.parse(localStorage.getItem("cart") ?? "[]");
+        console.log(productsOnCart)
+        const one = productsOnCart.find((each) => each.id === id);
+        console.log(one)
+        if (one) {
+            one.units = Number(units.current.value);
+            localStorage.setItem("cart", JSON.stringify(productsOnCart));
+        }
+    };
+
     return (
         <>
-            <div className="flex flex-grow basis-[60%] flex-col gap-[20px]">
-                <article className="flex flex-row items-center bg-slate-100 px-[20px] rounded justify-around flex-wrap "> 
-                    <img className="w-[100px] h-[100px] object-cover rounded mr-[20px]" src={image} alt="ipad" />
-                    <div className="flex flex-1 flex-col gap-[10px] p-4">
-                        <strong className="font-bold text-lg">{title}</strong><span className="text-gray-600 text-[15px] font-bold">- {color}</span>
-                        <p className="text-gray-600 text-[15px] font-bold"> Celular de alta gamma de 12.9 pulgadas.</p>
-                        <div className="text-gray-600 text-[15px] font-bold">
-                            <input className="padding-[5px] text-center w-[50px]"  type="number" name="quantity" value="1" min="1" id="P7Q8R90" />
-                        </div>
+
+            <article className="flex flex-row items-center bg-slate-100 px-[20px] rounded justify-around flex-wrap ">
+                <img className="w-[100px] h-[100px] object-cover rounded mr-[20px]" src={image} alt="ipad" />
+                <div className="flex flex-1 flex-col gap-[10px] p-4">
+                    <strong className="font-bold text-lg">{title}{id}</strong><span className="text-gray-600 text-[15px] font-bold">- {color}</span>
+                    <p className="text-gray-600 text-[15px] font-bold"> Celular de alta gamma de 12.9 pulgadas.</p>
+                    <div className="text-gray-600 text-[15px] font-bold">
+                        <input className="padding-[5px] text-center w-[50px]"
+                            id={id}
+                            type="number"
+                            name="quantity"
+                            defaultValue={quantity}
+                            ref={units}
+                            onChange={manageUnits}
+                            min="1"
+                        />
                     </div>
-                    <strong className="font-bold p-2 text-[18px] text-black sm:text-red-400 md:text-blue-500 lg:text-green-700 ">S/.{price}</strong>
-                </article>                                               
-            </div>
+                </div>
+                <strong className="font-bold p-2 text-[18px] text-black sm:text-red-400 md:text-blue-500 lg:text-green-700 ">S/.{price}</strong>
+            </article>
+
         </>
     )
 }
