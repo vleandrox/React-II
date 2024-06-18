b61ba2ef0b545eb852256714865abd51a147c119
vleandrox <victor.alarcon.t@hotmail.com>
2024-06-18 10:17:44 -0500

    UpdateAxios

--- virtual-store/src/components/OnSale.tsx
+++ virtual-store/src/components/OnSale.tsx
@@ -2,11 +2,13 @@ import styles from './OnSale.module.css'
 import Header from './Header'
 import Footer from './Footer'
 import OnSaleCard from './OnSaleCard'
-import products from '../assets/products.js'
-import { useState } from 'react'
+// import products from '../assets/products.js'
+import { useState,useEffect} from 'react'
+import axios from 'axios'
+import Product from '../interfaces/Product'
 
 export function OnSale() {
-    const onSaleProducts = products.filter((each) => each.onsale === true);
+    // const onSaleProducts = products.filter((each) => each.onsale === true);
     return (
         <>
             <Header></Header>
@@ -14,7 +16,7 @@ export function OnSale() {
                 <div>
                     <h2 className={styles["sales-title"]}>Ofertas de la semana</h2>
                     <div className={styles["onsale-container"]}>
-                        {onSaleProducts.map((each) => (
+                        {/* {onSaleProducts.map((each) => (
                             <OnSaleCard
                                 key={each.id}
                                 id={each.id}
@@ -23,7 +25,7 @@ export function OnSale() {
                                 color={each.colors[0]}
                                 image={each.images[0]}
                             />
-                        ))}
+                        ))} */}
                     </div>
                 </div>
             </main>
@@ -33,30 +35,42 @@ export function OnSale() {
 }
 
 export function OnSaleSlide() {
-    const onSaleProducts = products.filter((each) => each.onsale === true);
+    const [onsale, setOnSale] = useState<Product[]>([]); 
+    // const onSaleProducts = products.filter((each) => each.onsale === true);
+    useEffect(() => {
+        axios.get("/products.json")
+            .then((response) => {
+                const onsaleproducts = response.data.filter((each:Product) => each.onsale === true);
+                setOnSale(onsaleproducts);                
+            })
+            .catch((error) => console.log(error))
+    }, []);
+
+    // Generar el sliders del los productos en Venta
     const [from, setFrom] = useState(0);
     const [to, setTo] = useState(4);
     const nextSlide = () => {
-        if (to < onSaleProducts.length) {
+        if (to < onsale.length) {
             setFrom(from + 4);
             setTo(to + 4);
         }
     };
-
     const prevSlide = () => {
         if (from > 0) {
             setFrom(from - 4);
             setTo(to - 4);
         }
     };
-    const visibleProducts = onSaleProducts.slice(from, to);
+    const visibleProducts = onsale.slice(from, to);
+    // Finaliza el sliders del los productos en Venta
+
     return (
         <>
             <div className={styles["offer-container"]}>
                 <h2 className="font-bold text-[30px] underline">Ofertas de la semana</h2>
                 <button className={styles["prev-offer-button"]} onClick={prevSlide} disabled={from === 0}> {'<'} </button>
                 <div className={styles["offer-product-container"]}>
-                    {visibleProducts.map((each) => (
+                    {visibleProducts.map((each:Product) => (
                         <div className={styles["product-card"]} key={each.id}>
                             <OnSaleCard 
                                 id={each.id}
@@ -68,7 +82,7 @@ export function OnSaleSlide() {
                         </div>
                     ))}
                 </div>
-                <button className={styles["next-offer-button"]} onClick={nextSlide} disabled={to >= onSaleProducts.length}>{'>'}</button>
+                <button className={styles["next-offer-button"]} onClick={nextSlide} disabled={to >= onsale.length}>{'>'}</button>
                 {/* <div className={styles["slider-controls"]}>
                     <button onClick={prevSlide} disabled={from === 0}>{'Anterior'}</button>
                     <button onClick={nextSlide} disabled={to >= onSaleProducts.length}>{'Siguiente'}</button>
